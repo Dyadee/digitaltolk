eb5b362650c48efb5d6d58c41226b93f33b1f2ae
Jade Morales Feranndez <jadefernandez81@gmail.com>
2017-09-21 22:43:50 +0800

    refactoring the imports\aliasing in BookingRepository.php to make it shorter

--- code/refactor/app/Repository/BookingRepository.php
+++ code/refactor/app/Repository/BookingRepository.php
@@ -2,31 +2,17 @@
 
 namespace DTApi\Repository;
 
-use DTApi\Events\SessionEnded;
-use DTApi\Helpers\SendSMSHelper;
+use DTApi\Events\{SessionEnded, JobWasCreated, JobWasCanceled};
+use DTApi\Helpers\{SendSMSHelper, TeHelper, DateTimeHelper};
+use DTApi\Models\{Job, User, Language, UserMeta, UsersBlacklist, Translator, UserLanguages};
+use DTApi\Mailers\{AppMailer, MailerInterface};
+use Monolog\Handler\{StreamHandler, FirePHPHandler};
+use Illuminate\Support\Facades\{Log, Auth};
 use Event;
 use Carbon\Carbon;
 use Monolog\Logger;
-use DTApi\Models\Job;
-use DTApi\Models\User;
-use DTApi\Models\Language;
-use DTApi\Models\UserMeta;
-use DTApi\Helpers\TeHelper;
 use Illuminate\Http\Request;
-use DTApi\Models\Translator;
-use DTApi\Mailers\AppMailer;
-use DTApi\Models\UserLanguages;
-use DTApi\Events\JobWasCreated;
-use DTApi\Events\JobWasCanceled;
-use DTApi\Models\UsersBlacklist;
-use DTApi\Helpers\DateTimeHelper;
-use DTApi\Mailers\MailerInterface;
 use Illuminate\Support\Facades\DB;
-use Monolog\Handler\StreamHandler;
-use Illuminate\Support\Facades\Log;
-use Monolog\Handler\FirePHPHandler;
-use Illuminate\Support\Facades\Auth;
-
 /**
  * Class BookingRepository
  * @package DTApi\Repository
